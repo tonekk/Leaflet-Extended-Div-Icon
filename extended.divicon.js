ac8256033287da51dfb24d807090b48df79a0cb1
Finn Heemeyer <finn@heemeyer.net>
2014-11-19 15:19:48 +0100

    Updates source with closure

--- extended.divicon.js
+++ extended.divicon.js
@@ -1,21 +1,28 @@
-L.ExtendedDivIcon = L.DivIcon.extend({
-  createIcon: function(oldIcon) {
-    var div = L.DivIcon.prototype.createIcon.call(this, oldIcon);
-
-    if(this.options.id) {
-      div.id = this.options.id;
-    }
-
-    if(this.options.style) {
-      for(var key in this.options.style) {
-        div.style[key] = this.options.style[key];
+(function(L) {
+  
+  /*
+   * by tonekk. 2014, MIT License
+   */
+  
+  L.ExtendedDivIcon = L.DivIcon.extend({
+    createIcon: function(oldIcon) {
+      var div = L.DivIcon.prototype.createIcon.call(this, oldIcon);
+  
+      if(this.options.id) {
+        div.id = this.options.id;
       }
+  
+      if(this.options.style) {
+        for(var key in this.options.style) {
+          div.style[key] = this.options.style[key];
+        }
+      }
+  
+      return div;
     }
+  });
 
-    return div;
+  L.extendedDivIcon = function(options) {
+    return new L.ExtendedDivIcon(options);
   }
-});
-
-L.extendedDivIcon = function(options) {
-  return new L.ExtendedDivIcon(options);
-}
+})(window.L);
